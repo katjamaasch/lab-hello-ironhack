90904e868988ce1f2112f2518d298cdffe0b2fcb
Katja Maasch <katja.maasch@gmail.com>
2021-02-25 20:09:33 +0100

    Done, now with Component in the Components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import Iconcopytext from './Iconcopytext';
+import Iconcopytext from './Components/Iconcopytext';
 
 import './App.css';
 
