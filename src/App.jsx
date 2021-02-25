79b00fb47d3b015d7d3b39f4fcd9973ab4327b81
Katja Maasch <katja.maasch@gmail.com>
2021-02-25 19:49:38 +0100

    Implement basic design

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,59 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="logos">
+            <img
+              src="./images/ironhack-logo.svg"
+              className="Ironhack-logo"
+              alt="logo"
+            />
+            <img src="./images/menu-top.svg" className="App-logo" alt="logo" />
+          </div>
+          <div className="headline-and-copytext">
+            <h1 className="hello-react-headline">
+              Say hello to <br /> ReactJS
+            </h1>
+            <p>
+              You will learn a Frontend
+              <br />
+              framework from scratch, to <br />
+              become a Ninja Developer.
+            </p>
+            <form>
+              <button className="awesome">Awesome!</button>
+            </form>
+          </div>
         </header>
+        <div className="whitespace">
+          <div>
+            <img src="./images/icon1.png" className="Icons" alt="icon" />
+            <h3 className="icontext">Declarative</h3>
+            <p className="icontext">
+              React makes it painless to create interactive UIs.
+            </p>
+          </div>
+          <div>
+            <img src="./images/icon2.png" className="Icons" alt="icon" />
+            <h3 className="icontext">Components</h3>
+            <p className="icontext">
+              Build encapsulated components that manage their state.
+            </p>
+          </div>
+          <div>
+            <img src="./images/icon3.png" className="Icons" alt="icon" />
+            <h3 className="icontext">Single-Way</h3>
+            <p className="icontext">
+              A set of immutable values are passed to the component's.
+            </p>
+          </div>
+          <div>
+            <img src="./images/icon4.png" className="Icons" alt="icon" />
+            <h3 className="icontext">JSX</h3>
+            <p className="icontext">
+              Statically typed, designed to run on modern browsers.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
